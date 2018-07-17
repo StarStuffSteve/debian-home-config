eed49ff634eb4829b2e58f2f96c3b21ba1456d3d
Stephen Ennis <stephen.ennis@esa.int>
2018-07-17 10:09:34 +0200

    Updated setup script with tmux sourcing and ~ based paths

--- .setup_home.sh
+++ .setup_home.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Operating in " ~
+
 apt update && apt install -y \
   tmux \
   vim \
@@ -12,10 +14,11 @@ apt update && apt install -y \
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 
-rm -rf ./.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ./.tmux/plugins/tpm
-./.tmux/plugins/tpm/scripts/install_plugins.sh
+rm -rf ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
-rm -rf ./.vim/bundle/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ./.vim/bundle/Vundle.vim
+rm -rf ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
